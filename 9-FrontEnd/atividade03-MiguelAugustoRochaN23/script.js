b448d347e7d562ce47a40ff2d16cb256d648e3d6
Miguel Augusto <miguelaugustorocha2008@gmail.com>
2026-02-20 08:16:56 -0300

    Enviando Atividade 3 13/02/26

--- 9-FrontEnd/atividade03-MiguelAugustoRochaN23/script.js
+++ 9-FrontEnd/atividade03-MiguelAugustoRochaN23/script.js
@@ -7,32 +7,56 @@ function cadastrarAluno() {
     let ra = document.getElementById("ra").value;
     let telefone = document.getElementById("telefone").value;
     let turma = document.getElementById("turma").value;
+    let turmaAB = document.getElementById("turmaAB").value;
 
-    let lista = document.createElement("li");
-    lista.setAttribute("id", "aluno" + contadorAluno);
+    if (!validar()) {
+        contadorAluno--;
+        return;
+    }
 
-    lista.innerHTML =
+    document.getElementById("nome").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("ra").value = "";
+    document.getElementById("telefone").value = "";
+    document.getElementById("turma").value = "Fundamental I: 1°ano";
+    document.getElementById("turmaAB").value = "A";
+
+    let novoAluno = document.createElement("li");
+    novoAluno.setAttribute("id", "aluno" + contadorAluno);
+
+    novoAluno.innerHTML =
     "<strong>Aluno " + contadorAluno + "</strong>" +
-    "<ul class='dados-aluno'>" +
-    "<li>Nome: " + nome + "</li>" +
-    "<li>Email: " + email + "</li>" +
-    "<li>RA: " + ra + "</li>" +
-    "<li>Telefone: " + telefone + "</li>" +
-    "<li>Turma: " + turma + "</li>" +
+    "<ul>" +
+    "<li class='dados'><b>Nome:</b> " + nome + "</li>" +
+    "<li class='dados'><b>Email:</b> " + email + "</li>" +
+    "<li class='dados'><b>RA:</b> " + ra + "</li>" +
+    "<li class='dados'><b>Telefone:</b> " + telefone + "</li>" +
+    "<li class='dados'><b>Turma:</b> " + turma + " " + turmaAB + "</li>" +
     "</ul>";
 
     let botaoExcluir = document.createElement("button");
     botaoExcluir.textContent = "Excluir";
+    botaoExcluir.setAttribute("class", "excluir");
     botaoExcluir.setAttribute("onclick", "removerAluno(" + contadorAluno + ")");
 
-    lista.appendChild(document.createTextNode(" "));
-    lista.appendChild(botaoExcluir);
+    novoAluno.appendChild(document.createTextNode(" "));
+    novoAluno.appendChild(botaoExcluir);
 
-    document.getElementById("listaAlunos").appendChild(lista);
-
-    document.getElementById("formCadastro").reset();
+    document.getElementById("listaAlunos").appendChild(novoAluno);
 }
 function removerAluno(idAluno) {
     var aluno = document.getElementById("aluno" + idAluno);
     document.getElementById("listaAlunos").removeChild(aluno);
+}
+function validar() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let ra = document.getElementById("ra").value;
+    let telefone = document.getElementById("telefone").value;
+
+    if (nome === "" || email === "" || ra === "" || telefone === "") {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    return true;
 }
